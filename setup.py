ad05825e0f444b9d56514e7169c8826182322045
Victor Li <32348970+victorli2002@users.noreply.github.com>
2025-05-26 22:07:22 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sportsbetpy",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Internal DSL for sports betting",
+    author="Victor Li",
+    author_email="lvca760@gmail.com",
+    url="https://github.com/victorli2002/SportsBettingDSL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
